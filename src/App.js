403bdbda23c61b1f7eaf2455af90884c6f014f30
Anna Poltieva <anna.poltieva@kennasecurity.com>
2023-01-16 11:17:19 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
+import {BrowserRouter} from "react-router-dom";
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <p>REACT APP</p>
+      <BrowserRouter>
+          <Routes>
+              <Route>
+                  <Route exact path="/" element={<Resources />} />
+                  <Route exact path="/about" element={<About />} />
+              </Route>
+          </Routes>
+      </BrowserRouter>
+
     </div>
   );
+}
+
+function Resources() {
+    return(
+        <p>Resources</p>
+    )
+}
+function About() {
+    return(
+        <p>About</p>
+    )
 }
 
 export default App;
